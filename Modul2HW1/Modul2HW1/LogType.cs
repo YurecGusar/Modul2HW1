a85e82ddf4808a6b272b4f972d877b748f1126f5
Yurii <leoonovn@gmail.com>
2021-07-04 16:29:11 +0300

    Добавил класс Logger, который содержит : метод генерации лога метод записи лога в файл реализацию паттерна Singleton

---
+++ Modul2HW1/Modul2HW1/LogType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul2HW1
+{
+    internal enum LogType
+    {
+        Info = 1,
+        Warning,
+        Error
+    }
+}
